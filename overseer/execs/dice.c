c77bdf7f064f00a010dc66ef58ee2fe9e6b2ad16
Cecil Hammett <cecil@staticanalysis.io>
2020-09-24 13:27:44 +1000

    Roll dice program, simple loop, and printing second number in list of arguments

--- overseer/execs/dice.c
+++ overseer/execs/dice.c
@@ -1,4 +1,35 @@
 //
 // Created by Cecil Hammett on 9/24/20.
 //
+#include <stdio.h>
+#include <stdlib.h>
 
+int rolls[71];
+
+void roll_dice(void) {
+    srand(102161);
+    for (int i = 0; i < 71; ++i) {
+        rolls[i] = 1 + rand() % 5;
+    }
+}
+
+int main(void) {
+
+    for (int i = 0; i < 71; i++) {
+        rolls[i] = 0xCAB202;
+    }
+
+    roll_dice();
+
+    //  Display contents of array rolls.
+    for (int i = 0; i < 71; i++) {
+        if (i > 0) {
+            printf(",");
+        }
+        printf("%d", rolls[i]);
+    }
+
+    printf("\n");
+
+    return 0;
+}
--- overseer/execs/loop.c
+++ overseer/execs/loop.c
@@ -2,3 +2,15 @@
 // Created by Cecil Hammett on 9/24/20.
 //
 
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    if (argc == 2) {
+        printf("The argument given is %s\n", argv[1]);
+    } else if (argc > 2) {
+        printf("Too many arguments have been given.\n");
+    } else {
+        printf("At least one argument is expected.\n");
+    }
+}
