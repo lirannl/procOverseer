f21cca314e26f5f2f636db386fa720034736d8fa
lirannl <liranpiade@gmail.com>
2020-09-24 12:54:40 +1000

    Made a sheep counter

---
+++ overseer/execs/sheepCounter.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void greetUser() {
+    printf("Hello user!");
+}
+
+int main(int argc, char *argv[]) {
+    greetUser();
+
+    int sleepPoint = rand() % 8192;
+    int sheep = 0;
+    while (sheep < sleepPoint)
+    {
+        printf("%d sheep, ", sheep);
+        sheep++;
+    }
+    printf("\n\nGood night!\n");
+}
