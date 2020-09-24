790b5780a8a347447e1debd2694bbbeb5eaafe9b
lirannl <liranpiade@gmail.com>
2020-09-24 13:16:23 +1000

    Added new lines and fixed segfault

--- overseer/execs/factorial.c
+++ overseer/execs/factorial.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int factorial(int n){
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    return n * factorial(n-1);
+int factorial(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    return n * factorial(n - 1);
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        printf("No number supplied.\n");
+        return 1;
+    }
     int number = atoi(argv[1]);
-    if (argc == 2 && number >= 0 && number < 13) {
+    if (argc == 2 && number >= 0 && number < 13)
+    {
         printf("The factorial of %d is %d.\n", number, factorial(number));
         return 0;
     }
-    else if (argc != 2) printf("Please input exactly one number as an argument.\n");
-    else printf("The number you have entered is invalid. It must be a positive integer up to 12.\n");
+    else if (argc != 2)
+        printf("Please input exactly one number as an argument.\n");
+    else
+        printf("The number you have entered is invalid. It must be a positive integer up to 12.\n");
     return 1;
 }
