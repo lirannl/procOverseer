d25131c8b1c6de62920da15f3df46bb9d0e5acba
Cecil Hammett <cecil@staticanalysis.io>
2020-10-22 20:45:46 +1000

    might of fixed msg receiving

--- overseer/jobHandler.c
+++ overseer/jobHandler.c
@@ -13,27 +13,26 @@
 #include "connectionMethods.h"
 #include "requestQueue.h"
 
-struct timer_data
-{
+struct timer_data {
     int timeout;
     int logfd;
     pid_t pid;
 };
 
 char *recvMessage(int);
+
 int tHandler(char **);
+
 void *killProc(void *);
 
-int handle_job(int fd)
-{
+int handle_job(int fd) {
     /* Call method to recieve array data */
     char *results = recvMessage(fd);
     char *args[MAX_ARGS];
     char *opts[(MAX_OPTIONALS * 2) + 1];
     int valid_input = 0;
     valid_input = interpret_input(results, args, opts);
-    if (!valid_input)
-    {
+    if (!valid_input) {
         fprintf(stderr, "Invalid input.\n");
         return 0;
     }
@@ -41,20 +40,17 @@ int handle_job(int fd)
     // -o handling
     int out;
     int oIndex = findElemIndex(opts, "-o");
-    if (oIndex != -1)
-    {
+    if (oIndex != -1) {
         out = open(opts[oIndex + 1], O_WRONLY | O_CREAT, 0666);
     }
     // -log handling
     int log = fileno(stdout);
     int lIndex = findElemIndex(opts, "-l");
-    if (lIndex != -1)
-    {
+    if (lIndex != -1) {
         log = open(opts[lIndex + 1], O_WRONLY | O_CREAT, 0666);
     }
     char *fullExec = uniteStrArr(args);
-    if (valid_input == 1)
-    {
+    if (valid_input == 1) {
         // Append the execs folder to the path
         char cmdPath[CMD_MAX_LENGTH + 10];
         strcpy(cmdPath, "\0"); // Clear the cmdPath var
@@ -64,16 +60,14 @@ int handle_job(int fd)
         pipe(fds);
         write(fds[1], "T", 1);
         pid_t childPid = fork();
-        if (!childPid)
-        {
+        if (!childPid) {
             executeFileStart(fullExec, log);
             free(fullExec);
             int stdoutBkp;
             int stderrBkp;
             dup2(fileno(stdout), stdoutBkp);
             dup2(fileno(stderr), stderrBkp);
-            if (oIndex != -1)
-            {
+            if (oIndex != -1) {
                 dup2(out, fileno(stdout));
                 dup2(out, fileno(stderr));
                 close(out);
@@ -85,9 +79,7 @@ int handle_job(int fd)
             write(fds[1], "F", 1);
             close(fds[1]);
             exit(1);
-        }
-        else
-        {
+        } else {
             pthread_t timer;
             struct timer_data tData;
             tData.timeout = timeout;
@@ -121,8 +113,7 @@ int handle_job(int fd)
         else if (!strcmp(args[0], "memkill"))
             memkillHandler(args);
     }
-    if (lIndex != -1)
-    {
+    if (lIndex != -1) {
         close(log);
     }
     free(results);
@@ -132,80 +123,67 @@ int handle_job(int fd)
     return 0;
 }
 
-char *recvMessage(int fd)
-{
+char *recvMessage(int fd) {
+    char chunk[500];
     char *msg;
-    uint32_t netLen;
-    int recvLen = recv(fd, &netLen, sizeof(netLen), 0);
-    if (recvLen != sizeof(netLen))
-    {
+    memset(chunk, 0, 500);  //clear the variable
+    int recvLen;
+
+    if ((recvLen = recv(fd, chunk, 500, 0)) < 0) {
         fprintf(stderr, "recv got invalid length value (got %d)\n", recvLen);
         exit(1);
+    } else {
+        printf("%s", chunk);
+        msg = malloc(sizeof(chunk) + 1);
+        for (int i = 0; i < recvLen; i++) {
+            msg[i] = chunk[i];
+        }
+        msg[sizeof(chunk) + 1] = '\0';
+        return msg;
     }
-    int len = ntohl(netLen);
-    msg = (char *)malloc(len + 1);
-    if (recv(fd, msg, len, 0) != len)
-    {
-        fprintf(stderr, "recv got invalid length msg\n");
-        exit(1);
-    }
-    msg[len] = '\0';
-
-    return msg;
 }
 
-void *req_handler(void *data)
-{
+void *req_handler(void *data) {
     struct request *a_request;
-    struct global *globalData = (struct global *)data;
+    struct global *globalData = (struct global *) data;
 
     /* lock the mutex, to access the requests list exclusively. */
     pthread_mutex_lock(&request_mutex);
 
-    while (!globalData->termination_triggered)
-    {
-        if (num_requests > 0)
-        {
+    while (!globalData->termination_triggered) {
+        if (num_requests > 0) {
             a_request = get_request();
-            if (a_request)
-            { /* got a request - handle it and free it */
+            if (a_request) { /* got a request - handle it and free it */
                 //TO DO - UNLOCCK MUTEX, CALL FUNCTION TO HANDLE REQUEST AND RELOCK MUTEX
                 pthread_mutex_unlock(&request_mutex);
                 handle_job(a_request->fd);
                 free(a_request);
                 pthread_mutex_lock(&request_mutex);
             }
-        }
-        else
-        {
+        } else {
             pthread_cond_wait(&got_request, &request_mutex);
         }
     }
     printf("Quitting thread...\n");
 }
 
-int tHandler(char **opts)
-{
+int tHandler(char **opts) {
     int tIndex = findElemIndex(opts, "-t");
-    if (tIndex != -1)
-    {
+    if (tIndex != -1) {
         return atoi(opts[tIndex + 1]);
-    }
-    else
+    } else
         return 10;
 }
 
-void *killProc(void *data)
-{
-    struct timer_data *args = (struct timer_data *)data;
+void *killProc(void *data) {
+    struct timer_data *args = (struct timer_data *) data;
     sleep(args->timeout);
     if (kill(args->pid, 0) == -1)
         return NULL;
     kill(args->pid, SIGTERM);
     logSig(args->pid, "SIGTERM", args->logfd);
     sleep(5);
-    if (kill(args->pid, 0) != -1)
-    {
+    if (kill(args->pid, 0) != -1) {
         kill(args->pid, SIGKILL);
         logSig(args->pid, "SIGKILL", args->logfd);
     }
